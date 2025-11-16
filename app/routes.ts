11bd4c60fdbb36119754811bde982016c4c29287
sthedsyl <sthefanedias04@gmail.com>
2025-11-16 18:38:12 -0300

    feat: conguração da rota

--- app/routes.ts
+++ app/routes.ts
@@ -4,4 +4,5 @@ export default [
 	index("routes/login/login.tsx"),
 	route("aluno", "routes/aluno/aluno.tsx"),
 	route("admin", "routes/admin/admin.tsx"),
+	route("professor", "routes/professor/professor.tsx"),
 ] satisfies RouteConfig;
